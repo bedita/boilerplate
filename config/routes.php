ff3b18ec553050e41abb3f61397e21d711453d98
batopa <batopa@gmail.com>
2015-05-19 16:54:11 +0200

    ups... fix router rule

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@
 $apiBaseUrl = Configure::read('api.baseUrl');
 if (!empty($apiBaseUrl) && is_string($apiBaseUrl)) {
     $apiBaseUrl = ($apiBaseUrl == '/') ? '/*' : $apiBaseUrl . '/*';
-    Router::connect($apiBaseUrl . '/*', array('controller' => 'api', 'action' => 'route'));
+    Router::connect($apiBaseUrl, array('controller' => 'api', 'action' => 'route'));
 }
 
 Router::connect('/*', array('controller' => 'pages', 'action' => 'route'));
